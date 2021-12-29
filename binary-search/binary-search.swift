92532cd5d422df5dbcc54a5927b98b1729aa48a1
srikanth_iOS <adavalli123@gmail.com>
2021-12-29 13:29:02 -0700

    Time: 489 ms (8.34%), Space: 14.1 MB (89.14%) - LeetHub

---
+++ binary-search/binary-search.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        guard nums.count > 0 else { return -1 }
+        var left = 0, right = nums.count - 1
+        
+        while left <= right {
+            let mid = left + (right - left)/2
+            
+            if target == nums[mid] { 
+                return mid
+            } else if target < nums[mid] {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+        
+        return -1
+    }
+}
