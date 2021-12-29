096e3fa8dc9b45a96610b8ad3b881bd18b58119d
srikanth_iOS <adavalli123@gmail.com>
2021-12-29 13:59:22 -0700

    Time: 714 ms (14.88%), Space: 18 MB (33.41%) - LeetHub

--- contains-duplicate/contains-duplicate.swift
+++ contains-duplicate/contains-duplicate.swift
@@ -1,13 +1,5 @@
 class Solution {
     func containsDuplicate(_ nums: [Int]) -> Bool {
         return Set(nums).count != nums.count
-//         var dict: [Int: Bool] = [:]
-        
-//         for num in nums { 
-//             if let value = dict[num] { return true }
-//             dict[num] = true
-//         }
-        
-//         return false
     }
 }
