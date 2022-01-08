58993e668812ad20effbd9dbb360210755632535
srikanth_iOS <adavalli123@gmail.com>
2022-01-07 21:51:27 -0700

    Time: 4 ms (100.00%), Space: 14.1 MB (78.95%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.swift
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.swift
@@ -17,7 +17,6 @@ class Solution {
     func sumOfLeftLeaves(_ root: TreeNode?, _ isLeft: Bool = false) -> Int {
         guard let root = root else { return 0 }
         if isLeft, root.left == nil, root.right == nil { return root.val }
-        
         let left = sumOfLeftLeaves(root.left, true)
         let right = sumOfLeftLeaves(root.right, false)
         return left + right
