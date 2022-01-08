3dcc1997d9857ddddc18a3fbf98b5e056bf4c0de
srikanth_iOS <adavalli123@gmail.com>
2022-01-08 09:07:42 -0700

    Time: 11 ms (33.66%), Space: 14.2 MB (9.90%) - LeetHub

---
+++ 257-binary-tree-paths/257-binary-tree-paths.swift
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+
+class Solution {
+    func binaryTreePaths(_ root: TreeNode?, _ path: String = "") -> [String] {
+        guard let root = root else { return [] }
+        var path = path; if !path.isEmpty { path += "->"  }
+        
+        if root.left == nil, root.right == nil { return [path + String(root.val)] }
+        
+        let left = binaryTreePaths(root.left, path + String(root.val))
+        let right = binaryTreePaths(root.right, path + String(root.val))
+        return left + right
+    }
+}
