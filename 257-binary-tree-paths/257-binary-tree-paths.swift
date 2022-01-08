cbdc9c3d98fb8b0733b2cacdfb872fac831b409d
srikanth_iOS <adavalli123@gmail.com>
2022-01-08 09:20:13 -0700

    Time: 22 ms (5.94%), Space: 14 MB (51.49%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.swift
+++ 257-binary-tree-paths/257-binary-tree-paths.swift
@@ -15,14 +15,36 @@
  */
 
 class Solution {
+//     func binaryTreePaths(_ root: TreeNode?, _ path: String = "") -> [String] {
+//         guard let root = root else { return [] }
+//         var path = path; if !path.isEmpty { path += "->"  }
+        
+//         if root.left == nil, root.right == nil { return [path + String(root.val)] }
+        
+//         let left = binaryTreePaths(root.left, path + String(root.val))
+//         let right = binaryTreePaths(root.right, path + String(root.val))
+//         return left + right
+//     }
+    
     func binaryTreePaths(_ root: TreeNode?, _ path: String = "") -> [String] {
         guard let root = root else { return [] }
-        var path = path; if !path.isEmpty { path += "->"  }
+        var stack = [(root: root, path: String(root.val))], arr: [String] = []
         
-        if root.left == nil, root.right == nil { return [path + String(root.val)] }
+        repeat { 
+            guard let pop = stack.popLast() else { return [] }
+            if pop.root.left == nil, pop.root.right == nil {
+                arr.append(pop.path)
+            }
+            
+            if let left = pop.root.left { 
+                stack.append((left, pop.path + "->" + String(left.val)))
+            }
+            
+            if let right = pop.root.right { 
+                stack.append((right, pop.path + "->" + String(right.val)))
+            }
+        } while !stack.isEmpty
         
-        let left = binaryTreePaths(root.left, path + String(root.val))
-        let right = binaryTreePaths(root.right, path + String(root.val))
-        return left + right
+        return arr
     }
 }
