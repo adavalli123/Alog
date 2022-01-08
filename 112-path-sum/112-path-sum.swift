f81504b500e7e48fb6eb6c0c0e6e01c3742c4a1c
srikanth_iOS <adavalli123@gmail.com>
2022-01-07 23:20:18 -0700

    Time: 32 ms (96.39%), Space: 14.6 MB (23.47%) - LeetHub

--- 112-path-sum/112-path-sum.swift
+++ 112-path-sum/112-path-sum.swift
@@ -14,11 +14,25 @@
  * }
  */
 class Solution {
+    // func hasPathSum(_ root: TreeNode?, _ targetSum: Int) -> Bool {
+    //     guard let root = root else { return false }
+    //     if root.left == nil, root.right == nil { return targetSum == root.val }
+    //     let left = hasPathSum(root.left, targetSum - root.val)
+    //     let right = hasPathSum(root.right, targetSum - root.val)
+    //     return left || right
+    // }
+    
     func hasPathSum(_ root: TreeNode?, _ targetSum: Int) -> Bool {
         guard let root = root else { return false }
-        if root.left == nil, root.right == nil { return targetSum == root.val }
-        let left = hasPathSum(root.left, targetSum - root.val)
-        let right = hasPathSum(root.right, targetSum - root.val)
-        return left || right
+        var stack: [(root: TreeNode, val: Int)] = [(root, root.val)]
+        
+        while !stack.isEmpty {
+            guard let (node, path) = stack.popLast() else { return false }
+            if node.left == nil, node.right == nil, path == targetSum { return true }
+            if let left = node.left { stack.append((left, path + left.val)) }
+            if let right = node.right { stack.append((right, path + right.val)) }
+        }
+        
+        return false
     }
 }
