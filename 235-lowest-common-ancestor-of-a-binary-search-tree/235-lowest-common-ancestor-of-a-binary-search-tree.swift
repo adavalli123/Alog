c02f02de57eadd30f09fbd52d2a50adad110c2e8
srikanth_iOS <adavalli123@gmail.com>
2022-01-08 08:06:37 -0700

    Time: 273 ms (6.02%), Space: 15.2 MB (16.87%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.swift
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.swift
@@ -13,30 +13,30 @@
  */
 
 class Solution {
-//     func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
-//         guard let root = root else { return nil }
-//         switch (root, p, q) {
-//             case (let root, .some(let p), .some(let q)) where root.val < min(p.val, q.val): 
-//                 return lowestCommonAncestor(root.right, p, q)
-//             case (let root, .some(let p), .some(let q)) where root.val > max(q.val, p.val): 
-//                 return lowestCommonAncestor(root.left, p, q)
-//             default: return root
-//         }
-        
-//         return nil
-//     }
-    
     func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
-        guard let root = root, let p = p, let q = q else { return nil }
-        var stack = [root]
-        
-        while !stack.isEmpty {
-            guard let pop = stack.popLast() else { return nil }
-            if max(p.val, q.val) < pop.val { stack.append(pop.left!) } 
-            else if min(p.val, q.val) > pop.val { stack.append(pop.right!) } 
-            else { return pop }
+        guard let root = root else { return nil }
+        switch (root, p, q) {
+            case (let root, .some(let p), .some(let q)) where root.val < min(p.val, q.val): 
+                return lowestCommonAncestor(root.right, p, q)
+            case (let root, .some(let p), .some(let q)) where root.val > max(q.val, p.val): 
+                return lowestCommonAncestor(root.left, p, q)
+            default: return root
         }
         
         return nil
     }
+    
+//     func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
+//         guard let root = root, let p = p, let q = q else { return nil }
+//         var stack = [root]
+        
+//         while !stack.isEmpty {
+//             guard let pop = stack.popLast() else { return nil }
+//             if max(p.val, q.val) < pop.val { stack.append(pop.left!) } 
+//             else if min(p.val, q.val) > pop.val { stack.append(pop.right!) } 
+//             else { return pop }
+//         }
+        
+//         return nil
+//     }
 }
