0fbba8f3e12b521194b5f044574a1651774dbb2b
srikanth_iOS <adavalli123@gmail.com>
2022-01-08 22:43:33 -0700

    Time: 726 ms (29.19%), Space: 15.7 MB (94.16%) - LeetHub

---
+++ 938-range-sum-of-bst/938-range-sum-of-bst.swift
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func rangeSumBST(_ root: TreeNode?, _ low: Int, _ high: Int, _ path: Int = 0) -> Int {
+        guard let root = root else { return path }
+        var path = 0, stack = [root]
+        
+        while !stack.isEmpty { 
+            guard let pop = stack.popLast() else { return 0 }
+            if (low ... high).contains(pop.val) { path = path + pop.val }
+            if let left = pop.left { stack.append(left) }
+            if let right = pop.right { stack.append(right) }
+        }
+        
+        return path
+    }
+}
