0a5b5452c2f957575377e54fc1f62d58307488a0
srikanth_iOS <adavalli123@gmail.com>
2022-01-09 10:53:47 -0700

    Time: 22 ms (5.36%), Space: 13.9 MB (73.34%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.swift
+++ 88-merge-sorted-array/88-merge-sorted-array.swift
@@ -1,30 +1,16 @@
 class Solution {
     func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-        var nums: [Int] = [], lStart = 0, rStart = 0
+        var k = m + n - 1
+        var m = m - 1
+        var n = n - 1
         
-        while lStart < m, rStart < n {
-            if nums1[lStart] < nums2[rStart] {
-                nums += [nums1[lStart]]
-                lStart += 1
-            } else if nums1[lStart] > nums2[rStart] {
-                nums += [nums2[rStart]]
-                rStart += 1
-            } else {
-                nums += [nums1[lStart]]
-                lStart += 1
-            }
-        }
-        
-        while lStart < m  {
-            nums += [nums1[lStart]]
-            lStart += 1
+        while m >= 0, n >= 0 {
+            if nums1[m] < nums2[n] { nums1[k] = nums2[n]; k -= 1; n -= 1 }
+            else if nums1[m] > nums2[n] { nums1[k] = nums1[m]; k -= 1; m -= 1 }
+            else { nums1[k] = nums2[n]; k -= 1; n -= 1 }
         } 
         
-        while rStart < n {
-            nums += [nums2[rStart]]
-            rStart += 1
-        } 
-        
-        nums1 = nums
+        while m >= 0 { nums1[k] = nums1[m]; k -= 1; m -= 1 }
+        while n >= 0 { nums1[k] = nums2[n]; k -= 1; n -= 1 }
     }
 }
