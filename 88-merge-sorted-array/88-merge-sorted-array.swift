65c2da85d09841a3d9e138b8a353e697a9349c9d
srikanth_iOS <adavalli123@gmail.com>
2022-01-09 10:54:00 -0700

    Time: 16 ms (11.99%), Space: 14.1 MB (54.10%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.swift
+++ 88-merge-sorted-array/88-merge-sorted-array.swift
@@ -10,7 +10,7 @@ class Solution {
             else { nums1[k] = nums2[n]; k -= 1; n -= 1 }
         } 
         
-        while m >= 0 { nums1[k] = nums1[m]; k -= 1; m -= 1 }
+        // while m >= 0 { nums1[k] = nums1[m]; k -= 1; m -= 1 }
         while n >= 0 { nums1[k] = nums2[n]; k -= 1; n -= 1 }
     }
 }
