d362a08905debf70a63b2a081f01a796d39c1d4d
srikanth_iOS <adavalli123@gmail.com>
2022-01-09 20:55:58 -0700

    Time: 451 ms (5.97%), Space: 15 MB (64.18%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.swift
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.swift
@@ -14,10 +14,27 @@
  * }
  */
 class Solution {
+    // func insertIntoBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
+    //     guard let root = root else { return TreeNode(val) }
+    //     if root.val < val { root.right = insertIntoBST(root.right, val) }
+    //     if root.val > val { root.left = insertIntoBST(root.left, val) }
+    //     return root
+    // }
+    
     func insertIntoBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
         guard let root = root else { return TreeNode(val) }
-        if root.val < val { root.right = insertIntoBST(root.right, val) }
-        if root.val > val { root.left = insertIntoBST(root.left, val) }
+        var cur: TreeNode? = root, parent: TreeNode?
+        
+        while cur != nil {
+            parent = cur
+            
+            if cur!.val > val { cur = cur?.left }
+            else { cur = cur?.right }
+        }
+        
+        if let parent = parent, parent.val > val { parent.left = TreeNode(val) } 
+        else { parent?.right = TreeNode(val) }
+        
         return root
     }
 }
