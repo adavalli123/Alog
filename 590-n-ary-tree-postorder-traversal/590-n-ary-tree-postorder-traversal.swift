e51a12f6a5ca5fe8a36ad9a43e1e80f64b5c27bf
srikanth_iOS <adavalli123@gmail.com>
2022-01-09 18:37:34 -0700

    Time: 53 ms (13.51%), Space: 14.8 MB (59.46%) - LeetHub

--- 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.swift
+++ 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.swift
@@ -11,15 +11,31 @@
  */
 
 class Solution {
-    func postorder(_ root: Node?, _ arr: [Int] = []) -> [Int] {
-    	guard let root = root else { return arr }
-        if root.children.isEmpty { return arr + [root.val] }
-        var output: [Int] = []
+//     func postorder(_ root: Node?, _ arr: [Int] = []) -> [Int] {
+//     	   guard let root = root else { return arr }
+//         if root.children.isEmpty { return arr + [root.val] }
+//         var output: [Int] = []
         
-        root.children.forEach { 
-            output += postorder($0, arr)
+//         root.children.forEach { 
+//             output += postorder($0, arr)
+//         }
+        
+//         return arr + output + [root.val]
+//     }
+    
+    func postorder(_ root: Node?) -> [Int] {
+        guard let root = root else { return [] }
+        var stack = [root], results: [Int] = []
+        
+        while !stack.isEmpty {
+            guard let pop = stack.popLast() else { return [] }
+            for child in pop.children { 
+                stack.append(child)
+            }
+            
+            results.append(pop.val)
         }
         
-        return arr + output + [root.val]
+        return results.reversed()
     }
 }
