99a84e1c7f671a555e636ed1446298ce65912c90
srikanth_iOS <adavalli123@gmail.com>
2022-01-10 21:37:37 -0700

    Time: 108 ms (15.56%), Space: 14.9 MB (82.96%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.swift
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.swift
@@ -15,13 +15,31 @@
  */
 class Solution {
     func kthSmallest(_ root: TreeNode?, _ k: Int) -> Int {
-        func inorder(_ root: TreeNode?) -> [Int] {
-            guard let root = root else { return [] }
-            let left = inorder(root.left)
-            let right = inorder(root.right)
-            return left + [root.val] + right
+//         func inorder(_ root: TreeNode?) -> [Int] {
+//             guard let root = root else { return [] }
+//             let left = inorder(root.left)
+//             let right = inorder(root.right)
+//             return left + [root.val] + right
+//         }
+        
+//         return inorder(root).sorted()[k-1]
+        
+        guard let root = root else { return 0 }
+        var stack = [root], k = k, cur: TreeNode? = root
+        
+        while !stack.isEmpty || cur != nil {
+            while cur != nil { 
+                stack.append(cur!)
+                cur = cur?.left
+            }
+            
+            guard let pop = stack.popLast() else { return 0 }
+            // print(pop.val)
+            if k == 1 { return pop.val }
+            k -= 1
+            cur = pop.right
         }
         
-        return inorder(root).sorted()[k-1]
+        return -1
     }
 }
