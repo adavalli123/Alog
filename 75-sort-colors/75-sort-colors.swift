84f59bd969a1b5e6b67a9d4ffb11a993707f53f1
srikanth_iOS <adavalli123@gmail.com>
2022-01-21 10:18:05 -0700

    Time: 7 ms (33.92%), Space: 14 MB (43.61%) - LeetHub

--- 75-sort-colors/75-sort-colors.swift
+++ 75-sort-colors/75-sort-colors.swift
@@ -1,5 +1,13 @@
 class Solution {
     func sortColors(_ nums: inout [Int]) {
-        nums = nums.sorted()
+        var red = 0, white = 0, blue = 0
+        
+        for num in nums {
+            if num == 0 { red += 1 }
+            else if num == 1 { white += 1 }
+            else { blue += 1 }
+        }
+        
+        nums = Array(repeating: 0, count: red) + Array(repeating: 1, count: white) + Array(repeating: 2, count: blue)
     }
 }
