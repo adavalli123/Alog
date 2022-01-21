065185a7ddb278d8caea326cfa76a7d8f0bb8419
srikanth_iOS <adavalli123@gmail.com>
2022-01-20 21:17:08 -0700

    Time: 373 ms (35.28%), Space: 15.2 MB (44.79%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.swift
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.swift
@@ -1,18 +1,19 @@
 class Solution {
     func subarraySum(_ nums: [Int], _ k: Int) -> Int {
-        var count = 0
+        var count = 0, sum = 0, map: [Int: Int] = [0: 1]
         
-        for (i, num) in nums.enumerated() {
-            var sum = 0
-            
-            for j in i ..< nums.count {
-                sum += nums[j]
-                if sum == k {
-                    count += 1
-                }
+        for num in nums {
+            sum += num
+            if let val = map[sum - k] {
+                count += val
             }
+            
+            map[sum] = (map[sum] ?? 0) + 1
         }
         
         return count
     }
 }
+
+// 1 3 4 6 7
+// 1 2 3
