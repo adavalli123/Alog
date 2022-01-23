e82aca354327f0304a79c127ffefe51707f40b3d
srikanth_iOS <adavalli123@gmail.com>
2022-01-23 14:01:15 -0700

    Time: 184 ms (11.54%), Space: 14.1 MB (69.23%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.swift
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.swift
@@ -4,9 +4,7 @@ class Solution {
         
         for (i, m) in mat.enumerated() {
             for (j, n) in m.enumerated() where i == j || m.count - 1 - i == j {
-                // if i == j || m.count - 1 - i == j {
-                    diagonalSum += n
-                // }
+                diagonalSum += n
             }
         }
         
