b0cb6ad6fe86f247b05ddd4d170eda384123d235
srikanth_iOS <adavalli123@gmail.com>
2022-01-23 19:41:30 -0700

    Time: 21 ms (6.58%), Space: 13.9 MB (71.05%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.swift
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.swift
@@ -4,28 +4,28 @@ class Solution {
         var counter = 1, row = 0, col = 0, rowE = n - 1, colE = n - 1
         
         while counter <= n * n {
-            for i in stride(from: col, through: colE, by: 1) where counter <= n * n  {
+            for i in stride(from: col, through: colE, by: 1) {
                 defer { counter += 1 }
                 mat[row][i] = counter
             }
             
             row += 1
             
-            for i in stride(from: row, through: rowE, by: 1) where counter <= n * n {
+            for i in stride(from: row, through: rowE, by: 1) {
                 defer { counter += 1 }
                 mat[i][colE] = counter
             }
             
             colE -= 1
             
-            for i in stride(from: colE, through: col, by: -1) where counter <= n * n  {
+            for i in stride(from: colE, through: col, by: -1) {
                 defer { counter += 1 }
                 mat[rowE][i] = counter
             }
             
             rowE -= 1
             
-            for i in stride(from: rowE, through: row, by: -1) where counter <= n * n {
+            for i in stride(from: rowE, through: row, by: -1) {
                 defer { counter += 1 }
                 mat[i][col] = counter
             }
