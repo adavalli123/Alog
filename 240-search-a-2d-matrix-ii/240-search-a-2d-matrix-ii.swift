6cfe884792d47c59b4582cdc6fe50b6e82f545b7
srikanth_iOS <adavalli123@gmail.com>
2022-01-23 20:12:47 -0700

    Time: 360 ms (50.97%), Space: 14.7 MB (87.10%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.swift
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.swift
@@ -1,8 +1,14 @@
 class Solution {
     func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
-        for m in matrix {
-            for i in m where i == target {
-                return true
+        var index = 0
+        
+        while index < matrix.count {
+            var left = 0, right = matrix[index].count - 1
+            if matrix[index][left] <= target, target <= matrix[index][right] {
+                for num in matrix[index] where num == target { return true }
+                index += 1
+            } else {
+                index += 1
             }
         }
         
