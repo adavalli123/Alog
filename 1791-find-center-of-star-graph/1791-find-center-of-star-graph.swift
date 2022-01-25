ae952bb8b570937532fbdce2be6fa4bd935d1b3d
srikanth_iOS <adavalli123@gmail.com>
2022-01-24 17:31:57 -0700

    Time: 1788 ms (30.77%), Space: 23.6 MB (61.54%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.swift
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.swift
@@ -1,14 +1,5 @@
 class Solution {
     func findCenter(_ edges: [[Int]]) -> Int {
-        var prev: [Int] = []
-        
-        for e in edges {
-            defer { prev = e }
-            for i in e where prev.contains(i) {
-                return i
-            }
-        }
-        
-        return 0
+        return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1]
     }
 }
