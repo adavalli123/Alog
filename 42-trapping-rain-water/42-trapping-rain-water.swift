0b40d23d90baa4c068cbfd33d977d2800f5d1beb
srikanth_iOS <adavalli123@gmail.com>
2022-01-28 18:41:37 -0700

    Time: 53 ms (54.24%), Space: 14.3 MB (51.13%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.swift
+++ 42-trapping-rain-water/42-trapping-rain-water.swift
@@ -1,5 +1,5 @@
 class Solution {
-    func trap(_ height: [Int]) -> Int {
+    /* func trap(_ height: [Int]) -> Int {
         var trap = 0
         
         var l = 0, r = 0
@@ -23,12 +23,44 @@ class Solution {
                 }
             }
             
-            // print((lmax, rmax, height[i]))
             if min(lmax, rmax) - height[i] > 0 {
                 trap += min(lmax, rmax) - height[i]
             }
         }
         
         return trap
+    } */
+    
+    func trap(_ height: [Int]) -> Int {
+        var ans = 0
+        var rStack = Array(repeating: 0, count: height.count)
+        var lStack = rStack
+        
+        for i in height.indices {
+            if i == 0 {
+                lStack[i] = height[i]
+                continue
+            }
+            
+            lStack[i] = max(lStack[i-1], height[i])
+        }
+        
+        for j in stride(from: height.indices.last!, through: 0, by: -1) {
+            if j == height.indices.last {
+                rStack[j] = height[j]
+                continue
+            }
+            
+            rStack[j] = max(rStack[j+1], height[j])
+        }
+        
+        for i in height.indices {
+            // print((lStack[i], rStack[i], height[i]))
+            // if min(lStack[i], rStack[i]) - height[i] > 0 {
+                ans += min(lStack[i], rStack[i]) - height[i]
+            // }
+        }
+        
+        return ans
     }
 }
