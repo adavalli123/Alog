2e172c5d2e3411a23606b0dc3790d2bcd5f66a8a
srikanth_iOS <adavalli123@gmail.com>
2022-01-30 13:46:12 -0700

    Time: 12 ms (88.96%), Space: 22 MB (17.79%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func longestCommonSubsequence(_ text1: String, _ text2: String) -> Int {
+        var dp: [[Int]] = Array(repeating: Array(repeating: 0, count: text2.count + 1), count: text1.count + 1)
+        var text1 = Array(text1), text2 = Array(text2)
+        for (index, ch) in text1.enumerated() {
+            for (i, ch2) in text2.enumerated() {
+                if ch == ch2 {
+                    dp[index+1][i+1] = dp[index][i] + 1
+                    continue
+                }
+                
+                dp[index+1][i+1] = max(dp[index][i+1], dp[index+1][i])
+            }
+        }
+        return dp[text1.count][text2.count]
+    }
+}
