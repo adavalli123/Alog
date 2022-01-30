2a75f14ea80c186bf52a95ce78de121645d8fe2e
srikanth_iOS <adavalli123@gmail.com>
2022-01-30 11:35:01 -0700

    Time: 3 ms (34.47%), Space: 13.7 MB (80.39%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.swift
@@ -0,0 +1,13 @@
+class Solution {
+    func climbStairs(_ n: Int) -> Int {
+        var dp = Array(repeating: 0, count: n + 1)
+        if n > 0 { dp[1] = 1 } 
+        if n > 1 { dp[2] = 2 }
+          
+        for i in stride(from: 3, through: n, by: 1) {
+            dp[i] = dp[i-1] + dp[i-2]
+        }
+        
+        return dp[n]
+    }
+}
