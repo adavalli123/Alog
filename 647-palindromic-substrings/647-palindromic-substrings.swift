427fc3787bfaf34f7d4e877f88f16a546f80c715
srikanth_iOS <adavalli123@gmail.com>
2022-01-30 19:43:38 -0700

    Time: 8 ms (43.21%), Space: 15.3 MB (9.88%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.swift
@@ -0,0 +1,26 @@
+class Solution {
+    func countSubstrings(_ s: String) -> Int {
+        var dp = Array(repeating: Array(repeating: false, count: s.count + 1), count: s.count + 1)
+        var s = Array(s), count = 0, l = 0, r = 0
+        
+        for i in s.indices {
+            dp[i][i] = true
+            count += 1
+        }
+        
+        for i in stride(from: s.count, through: 0, by: -1) {
+            for j in stride(from: i+1, to: s.count, by: 1) {
+                // print((i, j))
+                if s[i] == s[j], (dp[i+1][j-1] || j - i < 2) {
+                    dp[i][j] = true
+                    count += 1
+                    // if count < j - i + 1 {
+                        // count += j - i 
+                    // }
+                }
+            }
+        }
+        
+        return count
+    }
+}
