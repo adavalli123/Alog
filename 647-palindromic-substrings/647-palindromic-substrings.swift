789794632745c9d17720d92e8c3b3e0449474e7a
srikanth_iOS <adavalli123@gmail.com>
2022-01-30 19:45:48 -0700

    Time: 8 ms (43.21%), Space: 15.1 MB (12.35%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.swift
+++ 647-palindromic-substrings/647-palindromic-substrings.swift
@@ -10,10 +10,8 @@ class Solution {
         
         for i in stride(from: s.count, through: 0, by: -1) {
             for j in stride(from: i+1, to: s.count, by: 1) where s[i] == s[j] && (dp[i+1][j-1] || j - i == 1) {
-                // if s[i] == s[j], (dp[i+1][j-1] || j - i == 1) {
-                    dp[i][j] = true
-                    count += 1
-                // }
+                dp[i][j] = true
+                count += 1
             }
         }
         
