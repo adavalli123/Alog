8a79bfd8b227fa5c55d16f2136be3c234d683745
srikanth_iOS <adavalli123@gmail.com>
2022-01-31 22:03:31 -0700

    Time: 161 ms (45.66%), Space: 14.9 MB (8.85%) - LeetHub

--- 136-single-number/136-single-number.swift
+++ 136-single-number/136-single-number.swift
@@ -1,5 +1,5 @@
 class Solution {
     func singleNumber(_ nums: [Int]) -> Int {
-        return nums.reduce(into: [:]){ $0[$1, default: 0] += 1 }.filter { $0.value == 1 }.first?.key ?? -1
+        return nums.reduce(0, ^)
     }
 }
