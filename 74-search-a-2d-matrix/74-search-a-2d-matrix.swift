ae2fa58c1094d091a9e5ac10c728ba2e78b6d24d
srikanth_iOS <adavalli123@gmail.com>
2022-01-31 19:05:12 -0700

    Time: 20 ms (97.13%), Space: 14.8 MB (5.10%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.swift
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.swift
@@ -1,19 +1,29 @@
 class Solution {
     func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
-        let matrix = matrix.flatMap { $0 }
-        var left = 0, right = matrix.count - 1
-        
-        while left < right {
-            let mid = left + (right - left)/2
-            if matrix[mid] < target {
-                left = mid + 1
-            } else if matrix[mid] > target {
-                right = mid
-            } else if matrix[mid] == target {
-                return true
+        var matrix = matrix
+        for i in matrix.indices {
+            for j in matrix[i].indices {
+                if dfs(&matrix, i, j, target) {
+                    return true
+                }
             }
         }
         
-        return matrix[left] == target
+        return false
+    }
+    
+    func dfs(_ matrix: inout [[Int]], _ i: Int, _ j: Int, _ target: Int) -> Bool {
+        guard i >= 0, j >= 0, i < matrix.count, j < matrix[0].count, matrix[i][j] > -500 else { return false }
+        
+        if matrix[i][j] == target {
+            return true
+        }
+        
+        matrix[i][j] = -1000
+        let d = dfs(&matrix, i + 1, j, target)
+        let u = dfs(&matrix, i - 1, j, target)
+        let l = dfs(&matrix, i, j + 1, target)
+        let r = dfs(&matrix, i, j - 1, target)
+        return d || u || l || r
     }
 }
