9576edf3101c295658bb4c06d7c06010b077391e
srikanth_iOS <adavalli123@gmail.com>
2022-02-12 20:52:30 -0700

    Time: 84 ms (62.00%), Space: 14.2 MB (52.00%) - LeetHub

---
+++ 735-asteroid-collision/735-asteroid-collision.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func asteroidCollision(_ asteroids: [Int]) -> [Int] {
+        var stack: [Int] = []
+        
+        outerloop: for asteroid in asteroids {
+            innerloop: while !stack.isEmpty, stack.last! >= 0, asteroid < 0 { 
+                if stack.last! > abs(asteroid) { continue outerloop }
+                if stack.last == abs(asteroid) { stack.popLast(); continue outerloop }
+                stack.popLast()
+                continue innerloop
+            }
+            
+            stack.append(asteroid)
+        }
+        
+        return stack
+    }
+}
