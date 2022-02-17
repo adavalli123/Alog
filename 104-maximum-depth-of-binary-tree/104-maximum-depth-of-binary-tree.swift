4a6304ef372804e8a7648668b26f8dc3894397f1
srikanth_iOS <adavalli123@gmail.com>
2022-02-17 14:31:03 -0500

    Time: 20 ms (99.52%), Space: 15 MB (8.31%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.swift
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.swift
@@ -14,24 +14,48 @@
  * }
  */
 class Solution {
+//     func maxDepth(_ root: TreeNode?) -> Int {
+//         guard let root = root else { return 0 }
+//         var stack: [(TreeNode, Int)] = [(root, 1)]
+//         var maximum = 0
+        
+//         while !stack.isEmpty {
+//             guard let pop = stack.popLast() else { return 0 }
+//             maximum = max(pop.1, maximum)
+            
+//             if let left = pop.0.left {
+//                 stack.append((left, pop.1 + 1))
+//             }
+            
+//             if let right = pop.0.right {
+//                 stack.append((right, pop.1 + 1))
+//             }
+//         }
+        
+//         return maximum
+//     }
+    
     func maxDepth(_ root: TreeNode?) -> Int {
         guard let root = root else { return 0 }
-        var stack: [(TreeNode, Int)] = [(root, 1)]
-        var maximum = 0
+        var q = [root]
         
-        while !stack.isEmpty {
-            guard let pop = stack.popLast() else { return 0 }
-            maximum = max(pop.1, maximum)
+        var maxLevel = 0
+        
+        while q.isEmpty == false {
+            maxLevel += 1
             
-            if let left = pop.0.left {
-                stack.append((left, pop.1 + 1))
-            }
+            for i in 0 ..< q.count {
+                let dequeue = q.removeFirst()
+                if let left = dequeue.left {
+                    q.append(left)
+                }
             
-            if let right = pop.0.right {
-                stack.append((right, pop.1 + 1))
+                if let right = dequeue.right {
+                    q.append(right)
+                }
             }
         }
         
-        return maximum
+        return maxLevel
     }
 }
