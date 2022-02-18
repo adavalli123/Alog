b3aeec43d276a3bb17930a61445d3a4dd23b22aa
srikanth_iOS <adavalli123@gmail.com>
2022-02-17 20:53:14 -0500

    Time: 20 ms (7.94%), Space: 14.4 MB (9.52%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.swift
+++ 70-climbing-stairs/70-climbing-stairs.swift
@@ -1,13 +1,9 @@
 class Solution {
     func climbStairs(_ n: Int) -> Int {
+        guard n > 1 else { return 1 }
         var dp = Array(repeating: 0, count: n + 1)
-        if n > 0 {
-            dp[1] = 1
-        }
-        
-        if n > 1 {
-            dp[2] = 2
-        }
+        dp[1] = 1
+        dp[2] = 2
         
         for i in stride(from: 3, through: n, by: 1) {
             dp[i] = dp[i-1] + dp[i-2]
