9b25acd442ccc22332fb56dc3ab39c6036a11c96
srikanth_iOS <adavalli123@gmail.com>
2022-03-23 22:29:24 -0500

    Time: 119 ms (16.67%), Space: 14.3 MB (96.30%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.swift
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.swift
@@ -1,6 +1,6 @@
 class Solution {
     func findItinerary(_ tickets: [[String]]) -> [String] {
-            var dict: [String: [String]] = tickets.reduce(into: [:]) { $0[$1[0], default: []] += [$1[1]] }.mapValues { $0.sorted().reversed() }
+            var dict: [String: [String]] = tickets.reduce(into: [:]) { $0[$1[0], default: []] += [$1[1]] }.mapValues { $0.sorted { $0 > $1 } }
     var stack: [String] = ["JFK"], result: [String] = []
     
     print(dict)
