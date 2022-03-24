22712201ee7a5111bc7e6537d2534b1952db13c1
srikanth_iOS <adavalli123@gmail.com>
2022-03-23 22:30:21 -0500

    Time: 75 ms (75.93%), Space: 14.5 MB (74.07%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.swift
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.swift
@@ -1,18 +1,17 @@
 class Solution {
     func findItinerary(_ tickets: [[String]]) -> [String] {
-            var dict: [String: [String]] = tickets.reduce(into: [:]) { $0[$1[0], default: []] += [$1[1]] }.mapValues { $0.sorted { $0 > $1 } }
-    var stack: [String] = ["JFK"], result: [String] = []
-    
-    // print(dict)
-    while !stack.isEmpty {
-        guard let pop = stack.last else { return [] }
-        if dict[pop] != nil, dict[pop]!.count != 0 {
-            stack.append(dict[pop]!.popLast()!)
-        } else {
-            result.append(stack.popLast()!)
+        var dict: [String: [String]] = tickets.reduce(into: [:]) { $0[$1[0], default: []] += [$1[1]] }.mapValues { $0.sorted { $0 > $1 } }
+        var stack: [String] = ["JFK"], result: [String] = []
+        
+        while !stack.isEmpty {
+            guard let pop = stack.last else { return [] }
+            if dict[pop] != nil, dict[pop]!.count != 0 {
+                stack.append(dict[pop]!.popLast()!)
+            } else {
+                result.append(stack.popLast()!)
+            }
         }
-    }
     
-    return result.reversed()
+        return result.reversed()
     }
 }
