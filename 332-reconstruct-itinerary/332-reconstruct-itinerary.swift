cee13dca3925ca2ba7afaff0250d2279d1cc894d
srikanth_iOS <adavalli123@gmail.com>
2022-03-23 22:29:32 -0500

    Time: 80 ms (68.52%), Space: 14.5 MB (74.07%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.swift
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.swift
@@ -3,7 +3,7 @@ class Solution {
             var dict: [String: [String]] = tickets.reduce(into: [:]) { $0[$1[0], default: []] += [$1[1]] }.mapValues { $0.sorted { $0 > $1 } }
     var stack: [String] = ["JFK"], result: [String] = []
     
-    print(dict)
+    // print(dict)
     while !stack.isEmpty {
         guard let pop = stack.last else { return [] }
         if dict[pop] != nil, dict[pop]!.count != 0 {
