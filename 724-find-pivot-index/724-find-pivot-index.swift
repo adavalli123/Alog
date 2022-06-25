38745a71621df068999b86282ec4de950757da54
srikanth_iOS <adavalli123@gmail.com>
2022-06-25 18:53:57 -0500

    Time: 127 ms (85.38%), Space: 14.5 MB (17.92%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.swift
+++ 724-find-pivot-index/724-find-pivot-index.swift
@@ -1,16 +1,27 @@
 class Solution {
     func pivotIndex(_ nums: [Int]) -> Int {
-        var sum = nums.reduce(0, +)
         var leftSum = 0
+        var rightSum = 0
         
-        for (index, num) in nums.enumerated() {
-            if leftSum == sum - leftSum - num {
-                return index
-            }
-            
-            leftSum += num
+        for i in 0 ..< nums.count {
+            rightSum += nums[i]
         }
         
+        for i in 0 ..< nums.count {
+            rightSum -= nums[i]
+            // print("1 st for loop : \((leftSum, rightSum))")
+            if leftSum == rightSum {
+                return i
+            }
+            
+            leftSum += nums[i]
+        }
+        
+        // if leftSum == rightSum {
+        //     return nums.count - 1
+        // }
+        
+        // print((leftSum, rightSum))
         return -1
     }
 }
