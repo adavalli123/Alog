db6f33491c3a2e86bd47adf8b9215d30717446c0
srikanth_iOS <adavalli123@gmail.com>
2022-06-25 17:24:14 -0500

    Time: 14 ms (84.73%), Space: 14.3 MB (28.69%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.swift
@@ -0,0 +1,10 @@
+class Solution {
+    func runningSum(_ nums: [Int]) -> [Int] {
+        var nums = nums
+        for i in 1 ..< nums.count {
+            nums[i] += nums[i - 1]
+        }
+        
+        return nums
+    }
+}
