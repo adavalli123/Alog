cdc1b1e817aa58e7d02c8e971b5ffe1c2f1dbe59
srikanth_iOS <adavalli123@gmail.com>
2022-07-07 21:43:31 -0500

    Time: 270 ms (78.79%), Space: 21.4 MB (61.08%) - LeetHub

--- 189-rotate-array/189-rotate-array.swift
+++ 189-rotate-array/189-rotate-array.swift
@@ -1,25 +1,6 @@
 class Solution {
     func rotate(_ nums: inout [Int], _ k: Int) {
-        guard nums.count > 1 else { return }
-        // output format    k
-        // 7 6 5 4 3 2 1    1
-        // 5 6 7 4 3 2 1    2
-        // 5 6 7 1 2 3 4    3
-        
-        // reverse array    index
-        // 1 2 3 4 5 6 7    N/A
-        // 7 2 3 4 5 6 1    0
-        // 7 6 3 4 5 2 1    1
-        // 7 6 5 4 3 2 1    2
-        // count - 7/2 > 3
-        
-        // even array   index
-        // 1 2 3 4 5 6  0
-        // 6 2 3 4 5 1  1
-        // 6 5 3 4 2 1  2
-        // 6 5 4 3 2 1  3
-        // count - 6/2 == 3
-        
+        // guard nums.count > 1 else { return }
         let k = k % nums.count
         reverse(0, nums.count-1, &nums)
         reverse(k, nums.count-1, &nums)
